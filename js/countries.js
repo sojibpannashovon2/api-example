6aeb4895896d64664d59981b05d2d80b2b3bfedc
Md Arifur Rahman Sojib <sojibkhanarif999@gmail.com>
2022-09-02 01:37:58 +0600

    advance

--- js/countries.js
+++ js/countries.js
@@ -17,8 +17,31 @@ const dataOfCountries = countries => {
         <p> name: ${country.name.common} <====> official: ${country.name.official} </p>
         <p> region: ${country.region} </p>
         <p> capital: ${country.capital}  </p>
-`;
+        <button onclick="countryD2etails('${country.cca2}')">details</button>
+          `;
         div.appendChild(newElement);
     })
+}
+// onclick = "userDetails('${country.cca3}')"
+const countryD2etails = (code) => {
+    // console.log('Get the country Code ', code);
+    const url = ` https://restcountries.com/v3.1/alpha/${code}`;
+    // console.log(url)
+    fetch(url)
+        .then(res => res.json())
+        .then(data => anotherDetails(data[0]))
+}
+
+const anotherDetails = code2 => {
+    console.log(code2);
+    const newDiv = document.getElementById('details-container');
+    const anotherDiv2 = document.createElement('div');
+    anotherDiv2.classList.add('countryStyle2');
+
+    anotherDiv2.innerHTML = `
+    <h3> population: ${code2.population}</h3>
+    <img src="${code2.flags.png}" alt="">
+    `;
+    newDiv.appendChild(anotherDiv2);
 }
 allCountries();
